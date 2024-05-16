d46f34357fde4f48f06f7a4193d08f9f7346d595
Samuel Muniz <137716288+DevSamuelMuniz@users.noreply.github.com>
2024-05-15 22:06:55 -0300

    Add files via upload

---
+++ pages/homePage/homePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+const HomePage = () => {
+  return (
+    <View>
+
+      <View>
+      <Text>Samuel Muniz</Text>
+      <Text>Idade: 22</Text>
+      <Text>Ocupação: Desenvolvedor frontend</Text>
+      <Text>Experiência: 1 ano de experiência...</Text>
+      </View>
+    </View>
+  );
+};
+
+export default HomePage;
